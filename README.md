388e5c52e7123302a65418b7a3381c168c548d5c
Matheus Rafael <mrsilva20122004@gmail.com>
2026-08-07 16:48:53 -0300

    feat: criação de states

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+- [Aula](https://www.youtube.com/watch?v=sqhXpFMbRK8)
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
+
 export function App() {
+  const [count, setCount] = useState(0)
+  const [hide, setHide] = useState(false)
 
   return (
-    <>
-      olá
-    </>
+    <div>
+
+      {hide && <p>TESTE</p>}
+      {!hide && <p>TESTE 2</p>}
+
+      <button onClick={() => setCount(count + 1)}>
+        {count}
+      </button>
+      <button onClick={() => setHide(!hide)}>
+        Toggle
+      </button>
+    </div>
   )
 }
